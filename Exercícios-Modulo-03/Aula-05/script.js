e7bd15c2eba5e32bf4c3b77edc0f9a29c7e6be83
SilasRSSsimoes <sillas.rss@gmail.com>
2022-09-15 15:41:43 -0300

    Aula 05

---
+++ Exercícios-Modulo-03/Aula-05/script.js
@@ -0,0 +1,84 @@
+//Exercíco 01//
+nomeDoFulano = "Yasmin"
+nomeDoBeltrano = "Guimarães"
+
+mesmoNome = nomeDoFulano == nomeDoBeltrano
+
+console.log(mesmoNome)
+
+
+
+//Exercíco 02//
+idade = 23
+maiorIdade = 18
+
+maiorDeIdade = (idade >= maiorIdade)
+
+console.log (maiorDeIdade)
+
+
+
+//Exercício 03//
+
+valorBoleto = 100
+valorComJuros = valorBoleto * 1.1
+
+console.log(valorComJuros)
+
+
+
+//Exercíco 04//
+
+item1 = 1
+item2 = 2
+item3 = 3
+item4 = 4 
+item5 = 5
+
+media = (item1 + item2 + item3 + item4 + item5) / 5
+
+console.log (media)
+
+
+
+//exercíco 05//
+
+vendas = 700
+custoDosProdutos = 400
+
+lucroBruto = (vendas - custoDosProdutos)
+
+margemBruta = (lucroBruto / vendas) 
+
+valorPercentual = margemBruta *100
+
+console.log(valorPercentual)
+
+
+
+//Exercício 06//
+
+saldoEmConta = 1000,00
+
+compraDaCalca = 99,90
+
+salario = 2500,00
+
+doacao = 0,10
+
+compraAr = (saldoEmConta - compraDaCalca + salario - doacao) * 0.75
+
+divida = (compraAr + compraDaCalca + doacao - saldoEmConta - salario) / 2
+
+saldoAtual = (saldoEmConta + salario - compraDaCalca - doacao - compraAr - divida)
+
+
+console.log(saldoAtual)
+
+
+
+
+
+
+
+
